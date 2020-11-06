e5cb24fb623ccd51adc7c49c00bcb516b76c35b0
eosmanspah2 <eosmanspah2@etf.unsa.ba>
2020-11-06 02:00:30 +0100

    Initial commit

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
-
-public class Supermarket {
+public class Supermarket{
+    private Artikl[] artikli=new Artikl[1000];
+    private int broj=0;
+    public boolean dodajArtikl(Artikl artikl){
+        if(broj<50){
+            artikli[broj]=new Artikl(artikl.getNaziv(), artikl.getCijena(), artikl.getKod());
+            broj++;
+            return true;
+        }
+        return false;
+    }
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+    private void pomjerajArtikle(int mjesto){
+        for(int i=mjesto; i<broj; i++){
+            artikli[i]=artikli[i+1];
+        }
+        artikli[broj]=null;
+        broj=broj-1;
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int ukupnaCijena=0;
+        for(int i=0; i<broj; i++){
+            ukupnaCijena=ukupnaCijena+artikli[i].getCijena();
+        }
+        return ukupnaCijena;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        for(int i=0; i<broj; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl ubaceni=new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i]=null;
+                pomjerajArtikle(i);
+                return ubaceni;
+            }
+        }
+        return null;
+    }
 }
